5a467155cad07c25490883e7aebde29629af32ed
Unknown <met914@gmail.com>
2018-05-05 17:30:41 +0300

    user model and location update

--- app/src/main/java/com/met/getticket/MainActivity.java
+++ app/src/main/java/com/met/getticket/MainActivity.java
@@ -26,13 +26,41 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final static String URL = "http://54.171.171.134:5000/";
+    private final static String GET_TICKET_URL = "http://54.171.171.134:5000/user";
+    private final static String UPDATE_LOCATION_URL = "http://54.171.171.134:5000/eta";
     private final String[] comboOptions = new String[]{"1", "2", "3", "4"};
+    private RequestQueue requestQueue;
+    private boolean clicked = false;
+    private User user;
+    private Random random = new Random();
+
     private final LocationListener mLocationListener = new LocationListener() {
         @Override
         public void onLocationChanged(final Location location) {
             Log.d("Latitude", Double.toString(location.getLatitude()));
             Log.d("Longtitude", Double.toString(location.getLongitude()));
+
+            if(clicked){
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, UPDATE_LOCATION_URL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Log.d("Response", response.toString());
+                            }
+                        },
+                        null) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> map = new HashMap<>();
+                        Random random = new Random();
+                        map.put("uid", Integer.toString(random.nextInt(1000)));
+                        map.put("eta", Integer.toString(random.nextInt(15)));
+
+                        return map;
+                    }
+                };
+                requestQueue.add(stringRequest);
+            }
         }
 
         @Override
@@ -57,31 +85,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        user = new User();
+        user.setUid(random.nextInt(Integer.MAX_VALUE)+"");
+
         final Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter adapter = new ArrayAdapter(this,
                 android.R.layout.simple_spinner_item, comboOptions);
         spinner.setAdapter(adapter);
+
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+
             return;
         }
+
         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000,
                 0, mLocationListener);
+
         final Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                clicked = true;
+                requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, GET_TICKET_URL,
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
@@ -92,11 +122,10 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     protected Map<String, String> getParams() {
                         Map<String, String> map = new HashMap<>();
-                        Random random = new Random();
-                        map.put("id", Integer.toString(random.nextInt(1000)));
+                        map.put("uid", user.getUid());
                         map.put("eta1", Integer.toString(random.nextInt(15)));
                         map.put("eta2", Integer.toString(random.nextInt(15)));
-                        map.put("type", spinner.getSelectedItem().toString());
+                        map.put("operation", spinner.getSelectedItem().toString());
                         return map;
                     }
                 };
---
+++ app/src/main/java/com/met/getticket/User.java
@@ -0,0 +1,26 @@
+package com.met.getticket;
+
+/**
+ * Created by met on 5/5/2018.
+ */
+
+public class User {
+    private String id;
+    private String sube;
+
+    public String getUid() {
+        return id;
+    }
+
+    public void setUid(String uid) {
+        this.id = uid;
+    }
+
+    public String getSube() {
+        return sube;
+    }
+
+    public void setSube(String sube) {
+        this.sube = sube;
+    }
+}
