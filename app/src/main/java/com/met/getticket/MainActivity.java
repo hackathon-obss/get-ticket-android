6b28ed18df7de47a61ec2ce194362720fc41934f
Görkem Mülayim <gorkemmulayim@icloud.com>
2018-05-05 14:35:41 +0300

    Fix http post request error.

--- app/src/main/java/com/met/getticket/MainActivity.java
+++ app/src/main/java/com/met/getticket/MainActivity.java
@@ -2,28 +2,31 @@ package com.met.getticket;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    private final static String GET_LINE_URL = "http://192.168.51.59/getLine";
-    private final static String GET_LINE_INFO_URL = "http://192.168.51.59/getLineInfo";
-    private String[] comboOptions;
+    private final static String URL = "http://54.171.171.134:5000/";
+    private final String[] comboOptions = new String[]{"1", "2", "3", "4"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        comboOptions = new String[4];
-        comboOptions[0] = "1";
-        comboOptions[1] = "2";
-        comboOptions[2] = "3";
-        comboOptions[3] = "4";
 
         final Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter adapter = new ArrayAdapter(this,
@@ -33,7 +36,27 @@ public class MainActivity extends AppCompatActivity {
         final Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                new Thread(new Worker(spinner.getSelectedItem().toString())).start();
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Log.d("Response", response.toString());
+                            }
+                        },
+                        null) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> map = new HashMap<>();
+                        Random random = new Random();
+                        map.put("id", Integer.toString(random.nextInt(1000)));
+                        map.put("eta1", Integer.toString(random.nextInt(15)));
+                        map.put("eta2", Integer.toString(random.nextInt(15)));
+                        map.put("type", spinner.getSelectedItem().toString());
+                        return map;
+                    }
+                };
+                requestQueue.add(stringRequest);
             }
         });
     }
