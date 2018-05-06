77075f647c5d9ba425f0b74705104ea760c06100
Unknown <met914@gmail.com>
2018-05-06 10:59:36 +0300

    son

--- app/src/main/java/com/met/getticket/InitialETACalculator.java
+++ app/src/main/java/com/met/getticket/InitialETACalculator.java
@@ -22,8 +22,8 @@ public class InitialETACalculator implements LocationListener {
     private final static String GET_ETA =
             "https://maps.googleapis.com/maps/api/directions/json?origin=%1$s&destination=%2$s"
                     + "&mode=%3$s";
-    private final static String SUBE_1_LOCATION = "41.089183, 29.005068";
-    private final static String SUBE_2_LOCATION = "41.083858, 29.004442";
+    private final static String SUBE_1_LOCATION = "41.089183,29.005068";
+    private final static String SUBE_2_LOCATION = "41.083858,29.004442";
     private final Context mContext;
     private final User mUser;
 
